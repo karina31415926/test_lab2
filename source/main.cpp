e974639a053754853ca82015ca02a8dac95e8a31
Karina Pikul <Karina.Pikul@sva.de>
2025-01-14 09:17:45 +0100

    change cut of

--- source/main.cpp
+++ source/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
 	auto load_universe_path = std::filesystem::path{"./NOTHING_TO_LOAD"};
 	bool output_intermediate_states = bool{true};
 	bool save_initial_universe = bool{true};
-	auto num_bodies = std::uint32_t{1000};
+	auto num_bodies = std::uint32_t{10000};
 	auto plot_intermediate_epochs = std::uint32_t{5};
 	auto plot_bounding_box_scale = std::uint32_t{5};
 	auto universe_generator = std::uint32_t{ 0 };
